5f9b8b5a8e21d08139e371b93c1d2377e0fabca5
paulohenriquefreitas <paulohfreitas_ti@hotmail.com>
2018-07-26 19:38:10 -0300

    Generico Metodos

---
+++ 2Disciplina/src/unidade3/grafico/GenericsApp.java
@@ -0,0 +1,17 @@
+package unidade3.grafico;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class GenericsApp {
+
+	public static void main(String[] args) {
+		List<Shape> lista = new ArrayList<>();
+		lista.add(new Circle());
+		lista.add(new Rectangle());
+		
+		List<?> ls =lista;
+
+	}
+
+}
