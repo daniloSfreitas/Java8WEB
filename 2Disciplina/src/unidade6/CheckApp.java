99755f94489cbbbb79e631ee7bdedad98f10bce4
paulohenriquefreitas <paulohfreitas_ti@hotmail.com>
2018-08-15 19:10:44 -0300

    LojaVirtuar

---
+++ 2Disciplina/src/unidade6/CheckApp.java
@@ -0,0 +1,23 @@
+package unidade6;
+
+import java.awt.Container;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
+
+import javax.swing.JCheckBox;
+import javax.swing.JFrame;
+
+public class CheckApp {
+
+	public static void main(String[] args) {
+		JFrame frame = new JFrame("Aplicação");
+        frame.setSize(350, 250);
+        Container cont = frame.getContentPane();
+        cont.setLayout(new FlowLayout());
+        cont.add(new JCheckBox("1-SIM"));
+        cont.add(new JCheckBox("2-NAO"));
+        frame.setVisible(true);
+
+	}
+
+}
